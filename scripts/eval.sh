5b0b4bf11bf8611dfb20d019e132647944be4d56
Shilin Yan <65387509+shilinyan99@users.noreply.github.com>
2024-06-26 10:00:14 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -12,25 +12,11 @@ DISTRIBUTED_ARGS="
     --master_addr $MASTER_ADDR \
     --master_port $MASTER_PORT
 "
-DATA_PATH=path/to/train
-EVAL_DATA_PATH=path/to/test
-
-OUTPUT_PATH=./results/xxx
-mkdir -p $OUTPUT_PATH
-
+PY_ARGS=${@:1}  # Any other arguments 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS main_finetune.py \
     --model AIDE \
-    --data_path $DATA_PATH \
-    --eval_data_path $EVAL_DATA_PATH \
     --batch_size 32 \
     --blr 5e-4 \
     --epochs 5 \
-    --warmup_epochs 0 \
-    --weight_decay 0 \
-    --num_workers 2 \
-    --reprob 0.25 \
-    --smoothing 0.1 \
-    --output_dir $OUTPUT_PATH \
-    --resume $1 \
-    --eval True \
+    ${PY_ARGS} \
 2>&1 | tee -a $OUTPUT_PATH/log_test.txt
